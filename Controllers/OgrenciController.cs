9d4af868f245aa83e4ba49f7fbb14bb14db23d4f
Test Kullanıcısı <Test@gmail.com>
2024-05-01 22:02:03 +0300

    gerekli değişiklikleri yaptım

--- Controllers/OgrenciController.cs
+++ Controllers/OgrenciController.cs
@@ -25,11 +25,99 @@ namespace EntityFremworkApp.Controllers
         return RedirectToAction("Index");
        }
 
+       [HttpGet]
+
        public async Task<IActionResult> Index()
        {
          return View(await  _context.Ogrenciler.ToListAsync());
        }
 
+       public async Task<IActionResult> Edit(int? id)
+       {
+        if(id==null)
+        {
+            //404 hata kodu gönderilir
+           return NotFound();
+        }
+
+
+         // veritabınında böyle bir Id yoksa 404 hatası gönderir
+        var ogr=await _context.Ogrenciler.FindAsync(id);
+
+         if(ogr ==null)
+         {
+            return NotFound();
+         }
+
+         return View(ogr);
+
+       }
+
+      [HttpPost]
+      [ValidateAntiForgeryToken] 
+      public async Task<IActionResult> Edit(int id,Ogrenci model)
+      {
+        if(id !=model.OgrenciId)
+        {
+             return NotFound();
+        }
+
+        if(ModelState.IsValid)
+        {
+           try{
+            _context.Update(model); 
+            //Günceleme kısmı burda olur
+            await _context.SaveChangesAsync();
+           }
+           catch(DbUpdateConcurrencyException)
+           {
+              if(!_context.Ogrenciler.Any(o => o.OgrenciId==model.OgrenciId))
+              {
+                return NotFound();
+              }
+              else{
+                throw;
+              }
+           }
+           return RedirectToAction("Index");
+        }
+
+        return View(model);
+      }
+
+      [HttpGet]
+      public async  Task<IActionResult>Delete(int? id)
+      {
+        if(id ==null)
+        {
+            return NotFound();
+        }
+
+        var ogrenci=await _context.Ogrenciler.FindAsync(id);
+
+        if(ogrenci==null)
+        {
+            return NotFound();
+        }
+
+        return View(ogrenci);
+      }
+
+      [HttpPost]
+
+      //form içerisindeki ıd aliyor
+      public async  Task<IActionResult>Delete([FromForm] int id)
+      {
+        var ogrenci=await _context.Ogrenciler.FindAsync(id);
+        if(ogrenci==null)
+        {
+            return NotFound();
+        }
+        _context.Ogrenciler.Remove(ogrenci);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+      }
 
     }
 }
